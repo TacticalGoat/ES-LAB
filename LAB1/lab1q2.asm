d9cb4077c9c857a76bbef68a17591e8f269aea73
Arjun Narayan <TacticalGoat@users.noreply.github.com>
2016-08-11 14:54:14 +0530

    Delete redundancy

--- LAB1/lab1q2.asm
+++ LAB1/lab1q2.asm
@@ -47,41 +47,6 @@ Reset_Handler
 	ADD R0,4
 	SUB R1,4
 	
-	LDR R2 ,[R0]
-	LDR R3 ,[R1]
-	STR R3,[R0]
-	STR R2,[R1]
-	ADD R0,4
-	SUB R1,4
-	
-	LDR R2 ,[R0]
-	LDR R3 ,[R1]
-	STR R3,[R0]
-	STR R2,[R1]
-	ADD R0,4
-	SUB R1,4
-	
-	LDR R2 ,[R0]
-	LDR R3 ,[R1]
-	STR R3,[R0]
-	STR R2,[R1]
-	ADD R0,4
-	SUB R1,4
-	
-	LDR R2 ,[R0]
-	LDR R3 ,[R1]
-	STR R3,[R0]
-	STR R2,[R1]
-	ADD R0,4
-	SUB R1,4
-	
-	LDR R2 ,[R0]
-	LDR R3 ,[R1]
-	STR R3,[R0]
-	STR R2,[R1]
-	ADD R0,4
-	SUB R1,4
-	
 
 STOP
 	B STOP
